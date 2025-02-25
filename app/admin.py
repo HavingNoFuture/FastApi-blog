0aeda6be7851526cd62394652b6a37d5db5f8ef3
Alexey Ermilov <alex.erm4@gmail.com>
2025-02-25 21:32:31 +0300

    Added admin panel

---
+++ app/admin.py
@@ -0,0 +1,20 @@
+from sqladmin import ModelView
+
+from app.models import Comment, Post, Subscription, User
+
+
+class UserAdmin(ModelView, model=User):
+    column_list = [User.id, User.email]
+
+
+class SubscriptionAdmin(ModelView, model=Subscription):
+    column_list = [Subscription.id, Subscription.user_id, Subscription.author_id]
+
+
+class PostAdmin(ModelView, model=Post):
+    column_list = [Post.id, Post.author_id]
+
+
+class CommentAdmin(ModelView, model=Comment):
+    column_list = [Comment.id, Comment.author_id]
+    is_async = True
--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from sqladmin import Admin
 
+from app.admin import CommentAdmin, PostAdmin, SubscriptionAdmin, UserAdmin
+from app.db import engine
 from app.routes import user_router
 from app.routes.auth import auth_router, pass_reset_router, register_router, verify_router
 from app.routes.comments import comments_router
@@ -20,3 +23,15 @@ app.include_router(subscriptions_router)
 
 app.include_router(posts_router)
 app.include_router(comments_router)
+
+
+# region Admin
+
+admin = Admin(app, engine)
+
+admin.add_view(UserAdmin)
+admin.add_view(SubscriptionAdmin)
+admin.add_view(PostAdmin)
+admin.add_view(CommentAdmin)
+
+# endregion Admin
